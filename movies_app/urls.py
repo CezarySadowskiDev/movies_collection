174ce966c08776f85de9de892f28212f07d8a770
Cezary Sadowski <battleczaro@gmail.com>
2024-11-17 20:16:53 +0100

    Major changes

--- movies_app/urls.py
+++ movies_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index, name="index"),
+    path("<slug:slug>", views.movie_details_page, name="movie_details")
 ]
--- movies_app/views.py
+++ movies_app/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from .models import Movies
+
+
+def get_movies_from_db():
+    movies = Movies.objects.all()
+    return movies
+
+
+def get_movie_from_db_by_slug(slug):
+    movie = Movies.objects.get(slug=slug)
+    return movie
 
 
 def index(request):
-    return render(request, "movies_app/index.html")
+    movies = get_movies_from_db()
+    return render(request, "movies_app/index.html", {
+        "movies": movies
+    })
+
+
+def movie_details_page(request, slug):
+    movie = get_movie_from_db_by_slug(slug)
+
+    return render(request, "movies_app/movie_details_page.html", {
+        "movie": movie
+    })
